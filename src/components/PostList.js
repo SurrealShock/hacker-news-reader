91cd976030ccf06803571a1ca6a1510be2249738
SurrealShock <Adrian_Avram@Outlook.com>
2018-08-16 20:51:13 -0700

    Performance improvements

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 import Post from './Post';
+import Pages from './navigation/Pages';
 
 export default class PostList extends Component {
   constructor(props) {
@@ -12,30 +13,40 @@ export default class PostList extends Component {
   }
 
   async componentDidMount() {
-    const { page } = this.props;
+    const { page } = this.props.match.params;
     const res = await Axios.get(
       'https://hacker-news.firebaseio.com/v0/topstories.json'
     );
 
-    res.data.slice((page - 1) * 20, page * 20).forEach(element => {
-      Axios.get(
-        `https://hacker-news.firebaseio.com/v0/item/${element}.json`
-      ).then(res => {
-        console.log('ran');
-        this.setState(prevState => ({
-          posts: [...prevState.posts, res.data]
-        }));
-      });
+    async function asyncForEach(array, callback) {
+      for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+      }
+    }
+
+    const newPosts = [];
+    await asyncForEach(
+      res.data.slice((page - 1) * 20, page * 20),
+      async element => {
+        const postData = await Axios.get(
+          `https://hacker-news.firebaseio.com/v0/item/${element}.json`
+        );
+        newPosts.push(postData.data);
+      }
+    );
+    this.setState({
+      posts: newPosts
     });
   }
 
   render() {
     return (
-      <div>
+      <React.Fragment>
         {this.state.posts.map(post => (
           <Post key={post.id} post={post} />
         ))}
-      </div>
+        <Pages page={this.props.match.params.page} />
+      </React.Fragment>
     );
   }
 }
